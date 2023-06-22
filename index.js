b622f9ae5d5c886910894e91f7a779afed05febf
Igor Gritsaenko <pds.beriev@gmail.com>
2023-06-22 12:34:00 +0300

    fix: add data from API

---
+++ index.js
@@ -0,0 +1,42 @@
+const avatar = document.querySelectorAll('.user_avatar');
+const login = document.querySelectorAll('.user_nickname');
+const node = document.querySelectorAll('.user_location--text');
+const url = document.querySelectorAll('.user_login');
+
+function getUsers() {
+    const array = fetch('https://api.github.com/users')
+    .then( data => data.json())
+    .then( data => {
+        let result = [];
+        for ( let i = 0; i <= 2; i++) {
+            let index = generateRandomNumber();
+            result.push(data[index]);
+        }
+
+        return result;
+    })
+    .then(result => {
+        for (let i = 0; i <= 2; i++) {
+            let loginAPI = result[i].login;
+            login[i].textContent = loginAPI;
+            let avatarAPI = result[i].avatar_url;
+            avatar[i].src = avatarAPI;
+            let nodeAPI = result[i].node_id;
+            node[i].textContent = nodeAPI;
+            let urlAPI = result[i].url;
+            url.href = urlAPI;
+        }
+    })
+}; 
+
+getUsers()
+
+// function getRandomUser() {
+//     fetch('https://api.github.com/users')
+//     .then( data => data.json())
+// }
+
+function generateRandomNumber () {
+    let number = Math.floor(Math.random() * 30);
+    return number;
+}
